b6834199c107611c938abf3677795fb61ebbe096
Chad Edwards <chadedwardsofficial@gmail.com>
2023-10-17 20:20:14 -0500

    created functions for both searching recent movies api, and search movies most popular

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var moviedbAPIkey = "ca2300a8fc07bd59f63e90f09c86ebb0";
 var youtubeAPIkey = "AIzaSyCcZRBi5jPVXYgEXJiuQVqYqqTcTR3pm2Y";
 var userInput = $("#input_text");
 var searchBtn = $("#searchBtn");
+var popularBtn = $('#popularBtn');
+var recentMoviesBtn =$('#recentMoviesBtn');
 // This const variable is for the authentication for the api key, it is passed at the end of the fetch url after a comma, this is a cleaner way of using our api key with parameters//
 const options = {
   method: "GET",
@@ -13,14 +15,32 @@ const options = {
   },
 };
 
-//Event Listeners//
+// Event Listeners //
 
 searchBtn.on("click", function (event) {
   event.preventDefault();
-  console.log("hello");
   getMoviedata(userInput.val());
   console.log(userInput.val());
 });
+
+popularBtn.on("click", function (event) {
+  event.preventDefault();
+  getPopularMovies();
+});
+
+
+recentMoviesBtn.on("click", function (event) {
+  event.preventDefault();
+  getRecentMovies();
+});
+
+
+
+
+
+//Functions//
+
+
 
 // This Function searches for movies with names based on the keywords submitted in the userInput//
 function getMoviedata(keyword) {
@@ -28,6 +48,43 @@ function getMoviedata(keyword) {
     `https://api.themoviedb.org/3/search/movie?query=${keyword}&page=1&include_adult=false&sort_by=popularity.desc`,
     options
   ) //This Fetch Url searches for Data from the api with the parameters of 1st page, Include Keyword, and sort by popularity in descending order//
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+
+
+// This Function searches for most popular movies currently, yielding 20 results per page //
+
+function getPopularMovies() {
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&per_page=20`,
+    options
+  ) 
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+
+
+
+// This function searches for the most recent movies, yielding 20 results per page //
+
+function getRecentMovies() {
+  fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&per_page=20`,
+    options
+  ) 
     .then(function (response) {
       console.log(response);
       return response.json();
