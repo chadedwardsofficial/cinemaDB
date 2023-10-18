67ce057d7d366ba6e071631e6a77fbf58a2e75c8
Chad Edwards <chadedwardsofficial@gmail.com>
2023-10-17 19:51:44 -0500

    added variables for both apikeys used. Created a Function to search for movie based on keyword used in the user Input, function ran when user clicks submit button

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
-var apikey = ca2300a8fc07bd59f63e90f09c86ebb0
+// Declared variables//
+var moviedbAPIkey = "ca2300a8fc07bd59f63e90f09c86ebb0";
+var youtubeAPIkey = "AIzaSyCcZRBi5jPVXYgEXJiuQVqYqqTcTR3pm2Y";
+var userInput = $("#input_text");
+var searchBtn = $("#searchBtn");
+// This const variable is for the authentication for the api key, it is passed at the end of the fetch url after a comma, this is a cleaner way of using our api key with parameters//
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTIzMDBhOGZjMDdiZDU5ZjYzZTkwZjA5Yzg2ZWJiMCIsInN1YiI6IjY1MmRkNDczYTgwMjM2MDBhYjNmZmE0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5F9sfg8eucxyyLQbigRXYnKSV97-kHFE2sR7m0xWNDo",
+  },
+};
+
+//Event Listeners//
+
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+  console.log("hello");
+  getMoviedata(userInput.val());
+  console.log(userInput.val());
+});
+
+// This Function searches for movies with names based on the keywords submitted in the userInput//
+function getMoviedata(keyword) {
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${keyword}&page=1&include_adult=false&sort_by=popularity.desc`,
+    options
+  ) //This Fetch Url searches for Data from the api with the parameters of 1st page, Include Keyword, and sort by popularity in descending order//
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
