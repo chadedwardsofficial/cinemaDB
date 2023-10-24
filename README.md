ce0fd3adddf3a0684e8ac43462add374e1f28ea1
kyungda <143377231+kyungda@users.noreply.github.com>
2023-10-23 19:20:45 -0500

    Adding new features

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# group-project1
+# CinemaDB
+
+
+
+## Table Of Contents
+
+
+
+
+##  Features
+
+- Search for movies by title, genre or release year.
+- View movie posters and trailers.
+- Mark and save your favorite movies.
+- Responsive desginf or an optimal viewing experience on both desktop and mobile devices.
+
+
+##
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 // Declared variables//
-
 var moviedbAPIkey = "ca2300a8fc07bd59f63e90f09c86ebb0";
 var youtubeAPIkey = "AIzaSyCcZRBi5jPVXYgEXJiuQVqYqqTcTR3pm2Y";
 var userInput = $("#input_text");
 var searchBtn = $("#searchBtn");
 var popularBtn = $("#popularBtn");
-var recentMoviesBtn = $("#recentMoviesBtn");
+var recentBtn = $("#recentBtn");
 var cardContainer = $(".cardContainer");
+var favoriteMovies = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
 // This const variable is for the authentication for the api key, it is passed at the end of the fetch url after a comma, this is a cleaner way of using our api key with parameters//
 const options = {
   method: "GET",
@@ -16,38 +16,45 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTIzMDBhOGZjMDdiZDU5ZjYzZTkwZjA5Yzg2ZWJiMCIsInN1YiI6IjY1MmRkNDczYTgwMjM2MDBhYjNmZmE0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5F9sfg8eucxyyLQbigRXYnKSV97-kHFE2sR7m0xWNDo",
   },
 };
-
 // Event Listeners //
-
 searchBtn.on("click", function (event) {
   event.preventDefault();
-  const userInputTrailer = userInput.val() + " trailer";
-  getMoviedata(userInput.val());
-  // getYoutubedata(userInputTrailer);
+  getMoviedata(userInput.val()); // This takes what the user types in the input field, and passes it as an argument for the getMovieData Function //
   console.log(userInput.val());
 });
-
 popularBtn.on("click", function (event) {
   event.preventDefault();
   getPopularMovies();
 });
-
-recentMoviesBtn.on("click", function (event) {
+recentBtn.on("click", function (event) {
   event.preventDefault();
   getRecentMovies();
 });
-
-cardContainer.on("click", ".movieLink", function(event){
+cardContainer.on("click", ".movieLink", function (event) {
+  // This on click function is designed for when you click the dynamically created button, it targets the specific parent card it is within and then runs the youtubeLink function and passes the movieTitle argument//
   event.preventDefault();
   console.log(event.target);
   var myBtn = $(event.target);
-  var movieTitle = myBtn.data('title') + ' trailer';
+  var movieTitle = myBtn.data("title") + " trailer";
   console.log(movieTitle);
   youtubeLink(movieTitle);
-
-})
+});
+$(".myFavoritesBtn").on("click", function (event) {
+  event.preventDefault();
+  getFavoriteMovies();
+});
+cardContainer.on("click", ".favoriteBtn", function (event) {
+  // This function takes the specific information within the parent card once you click the button and stores it in a variable which is then stored to local storage for later use//
+  event.preventDefault();
+  const card = $(this).closest(".card");
+  const movieInfo = {
+    title: card.find("span").eq(0).text(),
+    releaseDate: card.find("span").eq(1).text(),
+    posterPath: card.find("img").attr("src"),
+  };
+  saveToLocalStorage(movieInfo);
+});
 //Functions//
-
 // This Function searches for movies with names based on the keywords submitted in the userInput//
 function getMoviedata(keyword) {
   fetch(
@@ -60,71 +67,63 @@ function getMoviedata(keyword) {
     })
     .then(function (data) {
       console.log(data);
-      cardContainer.empty();
+      cardContainer.empty(); // Clears existing Container of any cards//
+      //A For Loop to go through the API results and dynamically create cards on the webpage with Titles and Images and Buttons//
       for (i = 0; i < 19; i++) {
-        // getYoutubedata(data.results[i].original_title);
-
         if (data.results[i].poster_path) {
           cardContainer.append(`  <div class="row">
-       
           <div class="card">
             <div class="card-content">
             <button class="movieLink" data-title="${data.results[i].original_title}">FontAwesomeIcon</button>
               <h5>Title: </h5><span>${data.results[i].original_title}</span>
               <h5>Release Date: </h5><span>${data.results[i].release_date}</span>
+              <button class="favoriteBtn"><i class="fa-solid fa-heart fa-beat" style="color: #ff0000;"></i></button>
             </div>
             <div class="card-image">
             <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}">
           </div>
           </div>
         </div>
-        
         `);
         }
       }
     });
 }
-
-// This Function searches for most popular movies currently, yielding 20 results per page //
-
+// This Function searches for most popular movies currently, yielding results per page //
 function getPopularMovies() {
   fetch(
     `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&per_page=10`,
     options
   )
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    cardContainer.empty();
-    for (i = 0; i < 19; i++) {
-      // getYoutubedata(data.results[i].original_title);
-
-      if (data.results[i].poster_path) {
-        cardContainer.append(`  <div class="row">
-     
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      cardContainer.empty();
+      for (i = 0; i < 19; i++) {
+        if (data.results[i].poster_path) {
+          //This if statement checks if the movie data has a poster img, it will publish the content on the page, this way we do not have cards with broken img icons//
+          cardContainer.append(`  <div class="row">
         <div class="card">
           <div class="card-content">
-          <a href="#">Link</a>
-            <h5>Title: </h5><span class="moviettl">${data.results[i].original_title}</span>
+          <button class="movieLink" data-title="${data.results[i].original_title}">FontAwesomeIcon</button>
+            <h5>Title: </h5><span>${data.results[i].original_title}</span>
             <h5>Release Date: </h5><span>${data.results[i].release_date}</span>
+            <button class="favoriteBtn">Favorite</button>
           </div>
           <div class="card-image">
           <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}">
         </div>
         </div>
       </div>
-      
       `);
+        }
       }
-    }
-  });
+    });
 }
-
-// This function searches for the most recent movies, yielding 20 results per page //
-
+// This function searches for the most recent movies, yielding results per page //
 function getRecentMovies() {
   fetch(
     `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&per_page=10`,
@@ -136,16 +135,33 @@ function getRecentMovies() {
     })
     .then(function (data) {
       console.log(data);
+      cardContainer.empty();
+      for (i = 0; i < 18; i++) {
+        if (data.results[i].poster_path) {
+          //This if statement checks if the movie data has a poster img, it will publish the content on the page, this way we do not have cards with broken img icons//
+          cardContainer.append(`  <div class="row">
+          <div class="card">
+            <div class="card-content">
+            <button class="movieLink" data-title="${data.results[i].original_title}"></button>
+              <h5>Title: </h5><span>${data.results[i].original_title}</span>
+              <h5>Release Date: </h5><span>${data.results[i].release_date}</span>
+              <button class="favoriteBtn"><i class="fa-solid fa-heart fa-beat" style="color: #ff0000;"></i></button>
+              <button class="PLACEHOLDER"><i class="fa-brands fa-youtube" style="color: #ff0000;"></i></button>
+            </div>
+            <div class="card-image">
+            <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}">
+          </div>
+          </div>
+        </div>
+        `);
+        }
+      }
     });
 }
-
 //This Function fetches data from Youtube's API with parameters of their trailer //
-
 function getYoutubedata(query) {
   const apiKey = "AIzaSyCcZRBi5jPVXYgEXJiuQVqYqqTcTR3pm2Y"; // Replace with your YouTube API key
-
   const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${query}&order=relevance&key=${apiKey}`;
-
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
@@ -159,9 +175,41 @@ function getYoutubedata(query) {
     })
     .catch((error) => console.error("Error:", error));
 }
-
-
-
-function openYoutube(){
-  window.open(`youtube.com/${data.resultsPLACEHOLDER}`, '_blank');
+function openYoutube() {
+  window.open(`youtube.com/${data.resultsPLACEHOLDER}`, "_blank");
 }
+// This function calls the Local Storage data of each card and appends it to the page//
+function getFavoriteMovies() {
+  const favoriteMovies =
+    JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+  cardContainer.empty();
+  for (const movie of favoriteMovies) {
+    cardContainer.append(`
+      <div class="row">
+        <div class="card">
+          <div class="card-content">
+            <h5>Title: </h5><span>${movie.title}</span>
+            <h5>Release Date: </h5><span>${movie.releaseDate}</span>
+          </div>
+          <div class="card-image">
+            <img src="${movie.posterPath}">
+          </div>
+        </div>
+      </div>
+    `);
+  }
+}
+function saveToLocalStorage(movie) {
+  const favoriteMovies =
+    JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+  favoriteMovies.push(movie);
+  localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+}
+
+
+
+
+
+
+
+
